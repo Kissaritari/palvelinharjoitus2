b3518ed7b84d11d1fdd969a58b7dae7e5429cd99
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2023-12-11 01:42:14 +0200

    vaiheessa

--- frontend/views/TodoView.tsx
+++ frontend/views/TodoView.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+import Todo from "Frontend/generated/com/example/application/Todo"
+import { TodoEndpoint } from "Frontend/generated/endpoints";
+import { TextField } from "@hilla/react-components/TextField.js";
+import { Button } from "@hilla/react-components/Button.js";
 export function TodoView() {
 
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [task, setTask] = useState('');
+
+    useEffect(()=> {
+        TodoEndpoint.findAll().then(setTodos);
+
+    },[]);
     return (
-        <div>
-            <h1>Kissa</h1>
+        <div className="p-m">
+            <h1>To-Do</h1>
+
+            <div className="flex gap-s">
+                <TextField/>
+                <Button theme="primary">Lisää </Button>
+            </div>
         </div>
 
 );
