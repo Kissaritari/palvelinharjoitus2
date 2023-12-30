b1fab8e706d494ae08b6bd3e461fc52842510503
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2023-12-30 23:15:22 +0200

    lisätty toimintaa

--- frontend/views/TodoView.tsx
+++ frontend/views/TodoView.tsx
@@ -11,14 +11,37 @@ export function TodoView() {
     useEffect(()=> {
         TodoEndpoint.findAll().then(setTodos);
     },[]);
+
+    async function addTodo() {
+        const saved = await TodoEndpoint.add(task);
+        if (saved){
+            setTodos([...todos,saved]);
+            setTask('');
+        }
+    }
+
+    async function updateTodo(todo:Todo, done: boolean) {
+        const saved = await TodoEndpoint.update({
+        ...todo,done
+        });
+        if(saved){
+        setTodos(todos.map(existing => existing.id === saved.id ? saved: existing))};
+    }
+
     return (
         <div className="p-m">
             <h1>To-Do</h1>
 
             <div className="flex gap-s">
                 <TextField value="{task}" onChange= {e => setTask(e.target.value)}/>
-                <Button theme="primary">Lisää </Button>
+                <Button theme="primary" onClick={addTodo}>Lisää </Button>
             </div>
+            {todos.map(todo => (
+                <div key={todo.id}>
+                <Checkbox checked={todo.done} onCheckedChanged={e => updateTodo(todo, e.detail.value)}/>
+                <span>{todo.task}</span>
+                </div>
+            )}
         </div>
 
 );
