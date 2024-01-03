04240642b3a060e970b48f47fa64b11244de62eb
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2024-01-03 15:14:34 +0200

    lisätty päivämäärätoimintoja

--- frontend/views/TodoView.tsx
+++ frontend/views/TodoView.tsx
@@ -4,17 +4,20 @@ import { TodoEndpoint } from "Frontend/generated/endpoints";
 import { TextField } from "@hilla/react-components/TextField.js";
 import { Checkbox } from '@hilla/react-components/Checkbox.js';
 import { Button } from "@hilla/react-components/Button.js";
+import { DatePicker } from '@hilla/react-components/DatePicker.js';
 export function TodoView() {
 
     const [todos, setTodos] = useState<Todo[]>([]);
     const [task, setTask] = useState('');
+    const [title, setTitle] = useState('');
+    const [dueDate, setDueDate] = useState('');
 
     useEffect(()=> {
         TodoEndpoint.findAll().then(setTodos);
     },[]);
 
     async function addTodo() {
-        const saved = await TodoEndpoint.add(task);
+        const saved = await TodoEndpoint.add(task,title);
         if (saved){
             setTodos([...todos,saved]);
             setTask('');
@@ -33,8 +36,17 @@ export function TodoView() {
         <div className="p-m">
             <h1>To-Do</h1>
 
-            <div className="flex gap-s">
-                <TextField value={task} onChange= {e => setTask(e.target.value)}/>
+            <div className="gap-s">
+                <div>
+                <TextField value={title} placeholder="Otsikko" onChange= {e => setTitle(e.target.value)}/>
+                </div>
+                <div>
+                <TextField value={task} placeholder="teksti" onChange= {e => setTask(e.target.value)}/>
+                <p></p>
+                <h3>Due date</h3>
+                <DatePicker value={dueDate} onChange={e => setDueDate(e.target.value)}></DatePicker>
+                </div>
+                
                 <Button theme="primary" onClick={addTodo}>Lisää </Button>
             </div>
             {todos.map(todo => (
